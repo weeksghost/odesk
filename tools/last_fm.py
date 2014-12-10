a510dce17935673af467e8c1343089483dcd3062
Erik Marty <emarty@broadway.com>
2014-12-10 18:23:55 -0500

    Working on output

--- tools/last_fm.py
+++ tools/last_fm.py
@@ -11,20 +11,33 @@ def get_tracks():
   j = json.loads(urllib.urlopen(url).read())
   results = j['similartracks']['track']
 
+  starget = {}
   tracks = {}
   for obj in results:
     song = re.sub(r'\W+', '_', obj['name']).lower()
     player = re.sub(r'\W+', '_', obj['artist']['name']).lower()
     song_id = "_".join([song, player])
+    starget['source'] = song_id
+
     tracks['match'] = obj['match']
     tracks['name'] = obj['name']
     tracks['artist'] = obj['artist']['name']
     tracks['playcount'] = obj['playcount']
     tracks['id'] = song_id
+    #nodes.append(tracks)
+
     data = json.dumps(tracks, indent=2, sort_keys=True)
+
+    #sources = json.dumps(starget, indent=2)
+    #data = json.dumps([tracks for tracks in nodes], indent=2)
+
+
     yield data
+    #yield sources
 
 track_generator = get_tracks()
 
+nodes = []
 for items in track_generator:
-  print items
+  nodes.append(items)
+  print nodes
