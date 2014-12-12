c4fd54a1e4b44fba774e6361b39a1e286e8361f1
Erik Marty <emarty@broadway.com>
2014-12-12 11:16:38 -0500

    Figured how to output consumed api data to custom output

--- tools/last_fm.py
+++ tools/last_fm.py
@@ -3,7 +3,7 @@
 import re
 import json
 import urllib
-
+import pickle
 
 def get_tracks():
   url = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=u2&track=With+or+Without+You&api_key=6b1730aec724adfbf0e21befc7385e6c&format=json'
@@ -12,9 +12,10 @@ def get_tracks():
   results = j['similartracks']['track']
 
   nodes = []
-  starget = {}
-  tracks = {}
+  links = []
   for obj in results:
+    tracks = {}
+    starget = {}
     song = re.sub(r'\W+', '_', obj['name']).lower()
     player = re.sub(r'\W+', '_', obj['artist']['name']).lower()
     song_id = "_".join([song, player])
@@ -25,25 +26,9 @@ def get_tracks():
     tracks['artist'] = obj['artist']['name']
     tracks['playcount'] = obj['playcount']
     tracks['id'] = song_id
-
-    data = json.JSONEncoder().encode(zip(tracks, starget))
-    nodes.append(data)
-    all_songs = json.dumps({'nodes': nodes}, indent=2)
+    nodes.append(tracks)
+    links.append(starget)
+    all_songs = json.dumps({'nodes': nodes, 'links': links}, indent=2)
   return all_songs
 
-    #sources = json.dumps(starget, indent=2)
-    #data = json.dumps(tracks, indent=2, sort_keys=True)
-    #data = json.dumps([tracks for tracks in nodes], indent=2)
-
-  #return tracks
-    #yield sources
-
 print(get_tracks())
-
-#track_generator = get_tracks()
-#nodes = []
-#for items in track_generator:
-#  #results = json.dumps(items, indent=2)
-#  nodes.append(items)
-#  #output = json.dumps({'nodes': nodes}, indent=2)
-#print json.dumps({'nodes': tracks}, indent=2)
