dcd0112b894569113b31529c385a4a505ac5ab81
Erik Marty <emarty@broadway.com>
2014-12-14 13:31:54 -0500

    Updated regex

--- tools/last_fm.py
+++ tools/last_fm.py
@@ -3,7 +3,6 @@
 import re
 import json
 import urllib
-import random
 
 def get_tracks():
   url = 'http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=u2&track=With+or+Without+You&api_key=6b1730aec724adfbf0e21befc7385e6c&format=json'
@@ -18,20 +17,19 @@ def get_tracks():
     starget = {}
     name = obj['name']
     artist = obj['artist']['name']
-    song_id = '_'.join([name, artist]).lower().replace(' ', '_')
-    song_id = re.sub(r'[/, (), __, \']+', '_', song_id)
-    song_id = re.sub(r'[\', .]', '', song_id)
-    song_id = re.sub(r'[^\x00-\x7F]+', 'no_data', song_id)
+    song_id = '_'.join([name, artist]).lower()
+    song_id = re.sub(r'[\s+]', '_', song_id)
+    song_id = re.sub(r'[\W+]', '', song_id)
     starget['source'] = "with_or_without_you_u2"
     starget['target'] = song_id
     tracks['match'] = float(obj['match'])
-    tracks['name'] = obj['name']
-    tracks['artist'] = obj['artist']['name']
+    tracks['name'] = name
+    tracks['artist'] = artist
     tracks['playcount'] = float(obj['playcount'])
     tracks['id'] = song_id
     nodes.append(tracks)
     links.append(starget)
     all_songs = json.dumps({'nodes': nodes, 'links': links}, indent=2)
-  return all_songs.strip()
+  return all_songs
 
 print(get_tracks())
